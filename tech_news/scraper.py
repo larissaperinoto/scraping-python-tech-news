eda7117105cfdd64563f72730bfa0770bc58f65c
Larissa Perinoto <perinotolarissa@gmail.com>
2023-03-14 08:55:44 +0100

    feat: implementa função fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,20 @@
+import requests
+import time
+
+
 # Requisito 1
-def fetch(url):
-    """Seu código deve vir aqui"""
+def fetch(url: str):
+    headers = {"user-agent": "Fake user-agent"}
+
+    try:
+        response = requests.get(url, headers=headers, timeout=3)
+        response.raise_for_status()
+        time.sleep(1)
+
+    except (requests.ReadTimeout, requests.HTTPError):
+        return None
+
+    return response.text
 
 
 # Requisito 2
